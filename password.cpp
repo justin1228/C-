db16ea514589914eaa5f02184f601191c648ac60
justin1228 <didls@hanyang.ac.kr>
2020-05-01 19:08:21 +0900

    add file

---
+++ password.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+void pass(int pw){
+    int tries = 0;
+    while(pw!=1234 && tries<3){
+        std::cout << "Wrong Number Try again: \n";
+        std::cin >> pw;
+
+    }
+    if(pw ==1234){
+        std::cout << "Correct! \n";
+    }
+}
+
+int main(){
+    int password;
+    std::cout << "Enter your P/W: ";
+    std::cin>> password;
+
+    pass(password);
+
+    return 0;
+}
