9c3ff0fca7094ca4ab3a2838a8a374005c82893c
justin1228 <didls1228@gmail.com>
2020-05-06 03:03:56 +0900

    add vector

---
+++ array.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(){
+    vector <int> v;
+    v.push_back (4);
+    v.push_back (6);
+    v.push_back (1);
+    v.push_back (86);
+
+    for(int i=0;i<v.size();i++){
+        cout << v[i]<< " ";
+    }
+
+    cout << "\n";
+   
+    return 0;
+
+    }
