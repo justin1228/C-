61f401aeb622ab741602c1a3b113334c2e1e575a
justin1228 <didls1228@gmail.com>
2020-05-06 10:35:48 +0900

    .

--- array.cpp
+++ array.cpp
@@ -1,20 +1,20 @@
 #include <iostream>
 #include <vector>
 
-using namespace std;
+
 
 int main(){
-    vector <int> v;
+    std::vector <int> v;
     v.push_back (4);
     v.push_back (6);
     v.push_back (1);
     v.push_back (86);
 
     for(int i=0;i<v.size();i++){
-        cout << v[i]<< " ";
+        std::cout << v[i]<< " ";
     }
 
-    cout << "\n";
+    std::cout << "\n";
    
     return 0;
 
--- pointer.cpp
+++ pointer.cpp
@@ -3,8 +3,11 @@
 int main(){
     int b = 4;
     int *a = &b;
+    std::string character = "Hello World!";
 
-    std::cout << a;
+    std::cout << a << std::endl;
+    std::cout << *a << std::endl;
+    std::cout << character << std::endl;
 
     return 0;
 }
